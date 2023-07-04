3b609baf22e028dc7423df29dfa4844f7aa0b6b9
Andrii Sachok <andriy.sachok@gmail.com>
2023-07-04 16:50:11 +0300

    connect CurrentUser query to <Header />

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import query from '../queries/CurrentUser';
 
 export class Header extends Component {
   render() {
+    console.log(this.props.data);
+    if (this.props.data.loading) {
+      return <p>Loading...</p>;
+    }
     return <div>Header</div>;
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
