b9763fa10550d33ce50040b4f52ed3b0434b46bd
Andrii Sachok <andriy.sachok@gmail.com>
2023-07-05 18:53:28 +0300

    added React client-side part

---
+++ client/components/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import mutation from '../mutations/Login';
+import query from '../queries/CurrentUser';
+import AuthForm from './AuthForm';
+import { hashHistory } from 'react-router';
+
+export class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { errors: [] };
+  }
+
+  componentWillUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      // redirect to Dashboard
+      hashHistory.push('/dashboard');
+    }
+  }
+
+  onSubmit({ email, password }) {
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }],
+      })
+      .catch((res) => {
+        const errors = res.graphQLErrors.map((err) => err.message);
+        this.setState({ errors });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Login</h3>
+        <AuthForm
+          errors={this.state.errors}
+          onSubmit={this.onSubmit.bind(this)}
+        />
+      </div>
+    );
+  }
+}
+
+export default graphql(query)(graphql(mutation)(LoginForm));
